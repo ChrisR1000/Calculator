f77fad3d2b7d82d504f1cd3e36a6dac9552ddcd8
Christopher Rojo <christopherrojo70@gmail.com>
2025-11-18 21:10:03 -0800

    Deleted a few lines

--- Calculator.py
+++ Calculator.py
@@ -16,17 +16,15 @@ class Calculator:
         result = number1 / number2
         print(result)
 
-    def multiply(self, number1, number2):
-        result = number1 * number2
-        print(result)
+    
     
     
 
-
+# test change
 
 
 calculator = Calculator(3, 2)
 calculator.add(3, 2)
-calculator.subtract(3, 2)
-calculator.multiply(3, 2)
+
+
 calculator.divide(3, 2)
