f2db8f7942092a3f617a8c8286e9e9475f8318ba
Christopher Rojo <christopherrojo70@gmail.com>
2025-11-18 21:14:49 -0800

    Added some new functions

--- Calculator.py
+++ Calculator.py
@@ -16,6 +16,10 @@ class Calculator:
         result = number1 / number2
         print(result)
 
+    def multiply(self, number1, number2):
+        result = number1 * number2
+        print(result)
+
     
     
     
@@ -25,6 +29,6 @@ class Calculator:
 
 calculator = Calculator(3, 2)
 calculator.add(3, 2)
-
-
+calculator.subtract(3,2)
+calculator.multiply(3,2)
 calculator.divide(3, 2)
